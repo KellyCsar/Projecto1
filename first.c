6d58e0fe2ab895d25cc1876538bfb3244119d148
KellyCsar <kellycesarr23@gmail.com>
2024-09-15 01:51:33 +0530

    Add files via upload

---
+++ first.c
@@ -0,0 +1,54 @@
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <math.h> 
+  
+
+ //Kelly
+
+     int main(){
+       //float a, b,c;
+        float delta, x1, x2, xv, yv , a, b, c;
+
+    printf("\t-------EQUACAO QUADRATICA-------\n\n\t");
+
+    printf("Digite o valor de a: ");
+    scanf("%f", &a);
+    printf("Digite o valor de b: ");
+    scanf("%f", &b);
+    printf("Digite o valor de c: ");
+    scanf("%f", &c);
+
+            // calcula o delta
+             //  delta  = b * b - 4 * a *c  ; 
+             if (delta < 0){
+               printf(" Nao possui raizes \n\t"); 
+               
+             } 
+              else if( delta >= 0 ){
+               x1 = -b/(2*a); 
+               //suporte de duas casa decimais
+               printf(" possui uma raiz onde  x =  %.2f\n" , x1);
+      
+             }else{
+               x1 = (-b + sqrt(delta))/(2*a);
+               //apresentacao da funcao 
+               
+               //quadratica e x negativo da  funcao
+         
+               x2 = (-b - sqrt(delta))/(2*a);
+
+               printf(" Os zeros da funcao: x1 = %.2f , x2 = %.2f\n\t" , x1 , x2);
+            
+            
+            }
+             
+             xv =  -b/( 2* a); 
+             yv = -delta /(4 *a);
+                return  0 ;
+
+
+               // === final feliz +++
+    
+ }  
+
+ 
